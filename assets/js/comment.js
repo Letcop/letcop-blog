94b5145b4c9f9198733515e827acd1317c398316
Jamshid91 <jamshid_tojiboyev@mail.ru>
2020-10-27 15:06:25 +0500

    added JS codes

---
+++ assets/js/comment.js
@@ -0,0 +1,22 @@
+const comments = document.querySelectorAll('.comments_group');
+
+comments.forEach((comment) => {
+  comment.addEventListener('mouseover', () => {
+  comment.children[2].classList.add('reply_hide');
+  comment.children[1].children[1].classList.add('comment_bgc')
+  })
+});
+
+comments.forEach((comment) => {
+  comment.addEventListener('mouseout', () => {
+    comment.children[2].classList.remove('reply_hide');
+    comment.children[1].children[1].classList.remove('comment_bgc')
+  })
+});
+
+
+function auto_grow(element) {
+  element.style.height = '5px';
+  element.style.height = (element.scrollHeight) + 'px';
+
+}
